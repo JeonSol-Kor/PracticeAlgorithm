2f70d1707db22ddb1fe3bc5049040ce96af5f232
JeonSol-Kor <134674363+JeonSol-Kor@users.noreply.github.com>
2024-04-30 13:29:04 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String ID = sc.next();
+		sc.close();
+		System.out.println(ID + "??!");
+	}
+}
