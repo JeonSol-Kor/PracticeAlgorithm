1de63e106b2fa0c39b9b2c6409c6b516f8c88fd3
JeonSol-Kor <134674363+JeonSol-Kor@users.noreply.github.com>
2024-05-09 10:19:31 +0900

    [Bronze II] Title: 문자열 반복, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -7,8 +7,5 @@ for i in range(T):
     R = int(R)
     output.append('')
     for j in range(len(S)):
-        if S[j] == '\\':
-            output[i] += S[j] * R * 2
-        else:
-            output[i] += S[j] * R
+        output[i] += S[j] * R
     print(output[i])
