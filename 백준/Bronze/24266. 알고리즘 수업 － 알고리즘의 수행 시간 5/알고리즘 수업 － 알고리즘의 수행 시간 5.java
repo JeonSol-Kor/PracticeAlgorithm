e70749268e7cd7f570c3e0b02d3d73bcf1d40f04
JeonSol-Kor <134674363+JeonSol-Kor@users.noreply.github.com>
2024-05-21 17:39:43 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 204 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		sc.close();
+		System.out.println(n * n * n);
+		System.out.println(3);
+	}
+}
