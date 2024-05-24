9e556fd850863fe106132055f7571487ee9ad03d
JeonSol-Kor <134674363+JeonSol-Kor@users.noreply.github.com>
2024-05-24 09:22:34 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 232 ms, Memory: 18376 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -11,25 +11,8 @@ public class Main {
 		int e = sc.nextInt();
 		int f = sc.nextInt();
 		sc.close();
-		int x;
-		int y;
-		if (a == 0) {
-			y = c / b;
-			x = (c * e - b * f) / (a * e - b * d);
-		} else if (b == 0) {
-			x = c / a;
-			y = (c * d - f * a) / (b * d - e * a);
-		} else if (d == 0) {
-			y = f / e;
-			x = (c * e - b * f) / (a * e - b * d);
-
-		} else if (e == 0) {
-			x = f / d;
-			y = (c * d - f * a) / (b * d - e * a);
-		} else {
-			y = (c * d - f * a) / (b * d - e * a);
-			x = (c - b * y) / a;
-		}
+		int x = (c * e - b * f) / (a * e - b * d);
+		int y = (c * d - f * a) / (b * d - e * a);
 		System.out.println(x + " " + y);
 	}
 }
