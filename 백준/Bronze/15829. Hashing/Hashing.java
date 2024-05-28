8457152f623df6e8cea91b5b00fba6c5cbe2c13d
JeonSol-Kor <134674363+JeonSol-Kor@users.noreply.github.com>
2024-05-28 12:27:09 +0900

    [Bronze II] Title: Hashing, Time: 208 ms, Memory: 17704 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,9 +11,23 @@ public class Main {
         long M = 0;
 
         for(int i = 0; i < L; i++){
-            M += (Hash.charAt(i) - 'a' + 1) * Math.pow(31, i);
+            M += (Hash.charAt(i) - 'a' + 1) * pow(31, i);
             M %= 1234567891;
         }
         System.out.println(M);
+    }
+
+    public static long pow(long base, long exponent) {
+        long result = 1;
+        while (exponent > 0) {
+            if (exponent % 2 == 1) {
+                result *= base;
+                result %= 1234567891;
+            }
+            base *= base;
+            base %= 1234567891;
+            exponent /= 2;
+        }
+        return result;
     }
 }
