4077cf8dd7c33a426b040cac3d967f5f58a33e0e
JeonSol-Kor <134674363+JeonSol-Kor@users.noreply.github.com>
2024-05-30 12:06:31 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2848 ms, Memory: 187368 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -0,0 +1,13 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+count = [0] * 20000001
+
+for _ in range(N):
+    num = int(sys.stdin.readline().strip())
+    count[num+1000000] += 1
+
+for i in range(20000001):
+    if count[i] > 0:
+        print(i-1000000)
