90d427a4e51f9528550b33908ee8f7d30b1d53c3
JeonSol-Kor <134674363+JeonSol-Kor@users.noreply.github.com>
2024-06-19 09:18:24 +0900

    [Bronze IV] Title: 기찍 N, Time: 880 ms, Memory: 33444 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+        for (int i = N; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
