224efb5f17364c356f6f9a3ce69d9a4f1cc50a10
JeonSol-Kor <134674363+JeonSol-Kor@users.noreply.github.com>
2024-06-25 13:40:47 +0900

    [Silver II] Title: 베르트랑 공준, Time: 196 ms, Memory: 18012 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -3,25 +3,38 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        int limit = 123456 * 2;
+        boolean[] isPrime = new boolean[limit + 1];
+
+        for (int i = 2; i <= limit; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i <= limit; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= limit; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
         while (true) {
             int n = Integer.parseInt(br.readLine());
-            if (n == 0)
+            if (n == 0) {
                 break;
-            int count = 0;
-            for(int i = n + 1; i <= 2 * n; i++){
-                boolean primeCheck = true;
-                for(int j = 2; j < (int)Math.sqrt((double)i) + 1; j++){
-                    if(i % j == 0){
-                        primeCheck = false;
-                        break;
-                    }
-                }
-                if(primeCheck) count++;
             }
-            answer.append(count).append("\n");
+
+            int count = 0;
+            for (int i = n + 1; i <= 2 * n; i++) {
+                if (isPrime[i]) {
+                    count++;
+                }
+            }
+            sb.append(count).append('\n');
         }
         br.close();
-        System.out.print(answer.toString());
+        
+        System.out.println(sb.toString());
     }
 }
