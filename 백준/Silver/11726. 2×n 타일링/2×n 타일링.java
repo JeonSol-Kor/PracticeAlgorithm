8fe7c315b6151aaeb9f132e4fdd53f42d604926d
JeonSol-Kor <134674363+JeonSol-Kor@users.noreply.github.com>
2024-08-27 14:06:44 +0900

    [Silver III] Title: 2×n 타일링, Time: 168 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        if(n == 1){
+            System.out.println(1);
+        }else if(n == 2){
+            System.out.println(2);
+        }else{
+            long[] dp = new long[n];
+            dp[0] = 1;
+            dp[1] = 2;
+            for(int i = 2; i < n; i++){
+                dp[i] = (dp[i-1]+ dp[i-2]) % 10007;
+            }
+            System.out.println(dp[n-1]);
+        }
+	}
+}
