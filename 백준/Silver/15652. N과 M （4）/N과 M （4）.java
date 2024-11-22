5ab7ef98101bd82daa9e60fc6ee8f2a00a57ef96
JeonSol-Kor <jeonsol1996@gmail.com>
2024-11-22 13:31:22 +0900

    [Silver III] Title: N과 M (4), Time: 108 ms, Memory: 15272 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static StringBuilder sb;
+    static int[] sequence;
+	public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        sc.close();
+
+        sb = new StringBuilder();
+        sequence = new int[M];
+        backTrack(1, 0);
+
+        System.out.println(sb.toString().trim());
+	}
+
+    private static void backTrack(int start, int depth) {
+        if (depth == M) {
+            for(int num : sequence){
+                sb.append(num).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i = start; i <= N; i++) {
+            sequence[depth] = i;
+            backTrack(i, depth + 1);
+        }
+    }
+}
