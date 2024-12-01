7ee8da455eade2dfb6b5e344dd2f2a3f231f4d00
JeonSol-Kor <jeonsol1996@gmail.com>
2024-12-01 16:12:59 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.05 ms, Memory: 68.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int age) {
+        StringBuilder sb = new StringBuilder();
+        String strAge = String.valueOf(age);
+        for(int i = 0; i < strAge.length(); i++){
+            sb.append((char)(strAge.charAt(i) + 49));
+        }
+        return sb.toString();
+    }
+}
