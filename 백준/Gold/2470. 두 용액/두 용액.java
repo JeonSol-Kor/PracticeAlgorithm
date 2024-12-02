f44c3d5adcdfd561fc394fa95ff8321eb2d1d0b6
JeonSol-Kor <jeonsol1996@gmail.com>
2024-12-02 13:15:06 +0900

    [Gold V] Title: 두 용액, Time: 384 ms, Memory: 37532 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] yongEak = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        Arrays.sort(yongEak);
+
+        if(yongEak[N-1] <= 0){      // 전부다 0 이하일 때
+            System.out.println(yongEak[N-2] + " " + yongEak[N-1]);
+        }else if(yongEak[0] >= 0){  // 전부다 0 이상일 때
+            System.out.println(yongEak[0] + " "  + yongEak[1]);
+        }else{
+            int num1 = 0, num2 = 0;
+            int left = 0, right = N - 1;
+            int min = Integer.MAX_VALUE;
+            
+            while (left < right){
+                int sum = yongEak[left] + yongEak[right];
+                if(sum == 0){
+                    num1 = yongEak[left];
+                    num2 = yongEak[right];
+                    break;
+                } else if (Math.abs(sum) < min) {
+                    min = Math.abs(sum);
+                    num1 = yongEak[left];
+                    num2 = yongEak[right];
+                } else {
+                    if(sum > 0) {
+                        right--;
+                    }else{
+                        left++;
+                    }
+                }
+            }
+            System.out.println(num1 + " " + num2);
+        }
+    }
+}
