f9c1f64f8dc1617dbaaf7208f653893af3cb3edb
JeonSol-Kor <jeonsol1996@gmail.com>
2024-12-04 11:17:38 +0900

    [level 0] Title: 합성수 찾기, Time: 0.03 ms, Memory: 95.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        boolean[] prime = new boolean[101];
+        for(int i = 2; i * i <= n; i++){
+            for(int j = i * i; j <= n; j += i){
+                prime[j] = true;
+            }
+        }
+        for(int i = 4; i <= n; i++){
+            if(prime[i]) answer++;
+        }
+        return answer;
+    }
+}
