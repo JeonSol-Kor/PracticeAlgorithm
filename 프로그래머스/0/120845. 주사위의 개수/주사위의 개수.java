6873d5b9cb2e400546d0c59a26f6e1cd95fe2a2e
JeonSol-Kor <jeonsol1996@gmail.com>
2024-12-04 11:12:20 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 1;
+        for(int side : box){
+            answer *= side / n;
+        }
+        return answer;
+    }
+}
