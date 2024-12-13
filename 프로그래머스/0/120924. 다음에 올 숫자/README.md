87d1c2e5e8b72875fbf22ef51a62521cb5b0fd4f
JeonSol-Kor <jeonsol1996@gmail.com>
2024-12-13 14:40:38 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/README.md
+++ 프로그래머스/0/120924. 다음에 올 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 다음에 올 숫자 - 120924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924?language=java) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 0.03 ms
+메모리: 83.5 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 13일 14:39:56
+2024년 12월 13일 14:40:36
 
 ### 문제 설명
 
--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,10 +1,6 @@
 class Solution {
     public int solution(int[] common) {
-        int cha = 0, bi = 0;
-        if(common[1] - common[0] == common[2] - common[1]) {
-            return common[common.length - 1] + common[1] - common[0];
-        }else{
-            return common[common.length - 1] * (common[1] / common[0]);
-        }
+        if(common[1] - common[0] == common[2] - common[1]) return common[common.length - 1] + common[1] - common[0];
+        else return common[common.length - 1] * (common[1] / common[0]);
     }
 }
