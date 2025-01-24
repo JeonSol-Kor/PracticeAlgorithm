d06df8ec017af941d018df9df24c630aab13b0ba
JeonSol-Kor <jeonsol1996@gmail.com>
2025-01-24 15:49:18 +0900

    [level 3] Title: 여행경로, Time: 1.74 ms, Memory: 85.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,19 +2,16 @@ import java.util.*;
 class Solution {
     public String[] solution(String[][] tickets) {
         Map<String, PriorityQueue<String>> graph = new HashMap<>();
-        for (String[] ticket : tickets){
+        for (String[] ticket : tickets) {
             graph.putIfAbsent(ticket[0], new PriorityQueue<>());
             graph.get(ticket[0]).offer(ticket[1]);
         }
-        
         LinkedList<String> result = new LinkedList<>();
-        
         dfs("ICN", graph, result);
         return result.toArray(new String[0]);
     }
-    
-    private void dfs(String current, Map<String, PriorityQueue<String>> graph, LinkedList<String> result){
-        while(graph.containsKey(current) && !graph.get(current).isEmpty()){
+    private void dfs (String current, Map<String, PriorityQueue<String>> graph, LinkedList<String> result) {
+        while(graph.containsKey(current) && !graph.get(current).isEmpty()) {
             String next = graph.get(current).poll();
             dfs(next, graph, result);
         }
