de1d10910e9f438eae2ef07a8ef3ee8f4f6b5ba6
JeonSol-Kor <jeonsol1996@gmail.com>
2025-02-05 09:11:51 +0900

    [level 1] Title: 최소직사각형, Time: 1.94 ms, Memory: 89.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int max = 0;
+        int min = 0;
+        for(int[] size : sizes){
+            max = Math.max(max, Math.max(size[0], size[1]));
+            min = Math.max(min, Math.min(size[0], size[1]));
+        }
+        return max * min;
+    }
+}   
