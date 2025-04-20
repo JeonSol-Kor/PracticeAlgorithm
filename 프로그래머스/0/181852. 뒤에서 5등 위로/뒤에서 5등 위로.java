e7a73127e6fcdc5048da8782f82c3863a46b0a49
JeonSol-Kor <jeonsol1996@gmail.com>
2025-04-20 12:55:55 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.56 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int len = num_list.length;
+        Arrays.sort(num_list);
+        int[] answer = new int[len-5];
+        for(int i = 5; i < len; i++){
+            answer[i-5] = num_list[i];
+        }
+        return answer;
+    }
+}
